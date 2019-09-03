559358ae97b2886a285822ee43251b4228efb9e0
mackacavs <mackenziegdixon@yahoo.com>
2019-09-03 16:39:30 +0100

    Letting players enter their names

--- spec/features/testing_infrastructure_spec.rb
+++
@@ -1,8 +0,0 @@
-
-
-feature "My battle application" do
-  scenario 'should allow access of the homepage' do
-    visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
-end
